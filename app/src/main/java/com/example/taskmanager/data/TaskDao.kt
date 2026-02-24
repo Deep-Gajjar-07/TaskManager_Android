62173ee5b40e3fec03f1df9597cebb6235f3d23a
Deep-Gajjar-07 <gajjardeep2017@gmail.com>
2026-02-24 14:53:51 +0530

    created Dao for Task CRUD operation

---
+++ app/src/main/java/com/example/taskmanager/data/TaskDao.kt
@@ -0,0 +1,25 @@
+package com.example.taskmanager.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTask(taskEntity: TaskEntity)
+
+    @Query("SELECT * FROM tasks")
+    fun getAllTask(): List<TaskEntity>
+
+    @Delete
+    fun deleteTask(taskEntity: TaskEntity)
+
+    @Update
+    fun updateTask(taskEntity: TaskEntity)
+
+}
